b06dc135518dda960b83ff144dd49f3e3756079a
sanjeevhalyal <sanjuuuu8@gmail.com>
2017-03-02 11:04:56 +0530

    Version 2

---
+++ app/src/main/java/nirva/mywedding/testactvitiyforgithub.java
@@ -0,0 +1,13 @@
+package nirva.mywedding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testactvitiyforgithub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testactvitiyforgithub);
+    }
+}
